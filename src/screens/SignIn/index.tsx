5e0cec29e4842ce28576197d06d5d06cf6ad3913
Eduardo Rodrigues de Farias <eduardo.rfarias@outlook.com>
2021-06-20 23:35:03 -0300

    v2

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,12 +1,34 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Image, Text, StatusBar } from 'react-native'
+import { ButtonIcon } from '../../components/ButtonIcon'
+import IllustrationImg from '../../assets/illustration.png'
 import { styles } from './style'
 
 export function SignIn() {
   return (
     <View style={styles.container}>
-      <Text>Olá mundo, NLW Together</Text>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode='stretch'
+      />
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize{'\n'}
+          suas jogatinas{'\n'}
+          facilmente
+        </Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games{'\n'}
+          favoritos com seus amigos
+        </Text>
+        <ButtonIcon title='Entrar com discord' activeOpacity={0.7} />
+      </View>
     </View>
   )
 }
